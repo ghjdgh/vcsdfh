880722e84589fd3a583effb8cd7d3bb47f5e6ca2
ghjdgh <44309871+ghjdgh@users.noreply.github.com>
2018-10-20 12:55:41 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyD6i1O1aeCS4e6HWkzcNJT9afrMoHmaYjI"));
+define('GJ_CODE', getenv("HK"));
+define('SITE_NAME',getenv("YouTobe"));
+define('TITLENAME', getenv("YouTobe"));
+define('EN2DEKEY',getenv("AgfSk3Afd1G"));
+define('EMAIL', getenv("ytu23@seznam.ce"));
 ?>
